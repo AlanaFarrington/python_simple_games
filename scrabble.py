cd940b868e46cde2e327dce109e16da7ab8695e1
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-08-09 22:51:40 +0100

    extension tasks added

--- scrabble.py
+++ scrabble.py
@@ -29,3 +29,4 @@ print(player_to_points)
 # play_word() — a function that would take in a player and a word, and add that word to the list of words they’ve played
 # update_point_totals() — turn your nested loops into a function that you can call any time a word is played
 # make your letter_to_points dictionary able to handle lowercase inputs as well
+
