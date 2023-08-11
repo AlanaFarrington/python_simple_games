a15b050baca7f70377881eb8feb402da8b57522e
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-08-11 21:10:49 +0100

    started new game

---
+++ hangman.py
@@ -0,0 +1,4 @@
+# draw hangman in code as a set of strings
+# set up a list containing the different strings for the building of the handman picture
+# game loop woth word to guess
+# take input from user
