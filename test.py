85fd7acaf79cbf78c0ca3562b82b1c57d1b67b17
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-08-11 22:55:46 +0100

    got basic game logic working

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import random
-
 rounds_played = 0
 game_choices = {1: "rock", 2: "paper", 3: "scissors"}
 player_stats = []
@@ -15,37 +14,35 @@ def compare_choices():
     computer_choice = random.randint(1,3)
     print("The computer chose " + game_choices[computer_choice])
     if player_choice == computer_choice:
-            print("You drew this round.\n")
-            # player_score += 1
-            # computer_score += 1
+            print("You drew this round.")
+            player_score += 1
+            computer_score += 1
             player_stats.append("drew")
             computer_stats.append("drew")
     elif (player_choice == 1 and computer_choice == 3) or (player_choice == 2 and computer_choice == 1) or (player_choice == 3 and computer_choice == 2):
-        print("You won!\n")
+        print("You won!")
         player_score += 1
         player_stats.append("won")
         computer_stats.append("lost")
     else:
-        print("You lost this round.\n")
+        print("You lost!")
         computer_score += 1
         player_stats.append("lost")
         computer_stats.append("won")
 
 # Make game loop run 3 times
-def game_loop(winning_total):
-    global rounds_played
-    while (player_score < winning_total) and (computer_score < winning_total):
+def game_loop():
+    while (player_score < 3) or (computer_score < 3):
         rounds_played += 1
         print("Round " + str(rounds_played))
         compare_choices()
 
-game_loop(3)
-
-# print final score list
-print("GAME OVER!\n")
-print("This game lasted " + str(rounds_played) + " rounds.\n")
-print("You scored " + str(player_score) + " and the computer scored " + str(computer_score) + ".\n")
+#print final score list
+print("Game over!")
+print("This game lasted " + str(rounds_played) + " rounds.")
+print("You scored " + str(player_score) + " and the computer scored " + str(computer_score))
 print("Your stats were:")
 print(player_stats)
 print("The computer's stats were:")
 print(computer_stats)
+
