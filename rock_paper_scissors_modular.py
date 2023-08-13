aba4f9ccafd302d3b6a83e2bb274365c26e34222
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-08-13 19:33:55 +0100

    corrected function issues and improved readability

--- rock_paper_scissors_modular.py
+++ rock_paper_scissors_modular.py
@@ -1,9 +1,6 @@
 import random
-
-rounds_played = 0
 game_choices = {1: "rock", 2: "paper", 3: "scissors"}
 
-# taking player input
 def get_player_input():
     return input("Make your choice. rock = 1, paper = 2 or scissors = 3? ")
 
@@ -25,53 +22,70 @@ def get_outcome(player_choice, computer_choice):
 
 def update_scores(scores, outcome):
     if outcome == 2:
-        scores["player_score"] +=1
+        scores["player"] +=1
     elif outcome == 3:
-        scores["computer_score"] +=1
+        scores["computer"] +=1
 
-# printing message to player
-def print_message():
-    if 
-    print(print("You chose " + game_choices[player_choice_int]))
-    
-    print("You drew this round.\n")
-    print("You won!\n")
-    print("You lost this round.\n")
+def is_game_complete(scores, winning_total):
+    if scores["player"] == winning_total:
+        return False
+    if scores["computer"] == winning_total:
+        return False
+    return True
+
+def print_choice_message(player, computer):
+    print("You chose " + game_choices[player])
+    print("The computer chose " + game_choices[computer])
+
+# print round outcome
+def print_outcome(outcome):    
+    match outcome:
+        case 1:
+            print("You drew this round.\n")
+        case 2:
+            print("You won!\n")
+        case 3:
+            print("You lost this round.\n")
+        case _:
+            print("invalid outcome error\n") 
 
 # adding stats
-def update_stats(outcome):
-    player_stats = []
-    computer_stats = []
+def update_stats(outcome, stats):
     if outcome == 1:
-        player_stats.append("drew")
-        computer_stats.append("drew")
+        stats["player"] += ["drew"]
+        stats["computer"] += ["drew"]
     elif outcome == 2:
-        player_stats.append("won")
-        computer_stats.append("lost")
+        stats["player"] += ["won"]
+        stats["computer"] += ["lost"]
     else:
-        player_stats.append("lost")
-        computer_stats.append("won")
-
-# run game loop
-def run_game():
-    scores = {"computer_score": 0, "player_score": 0}
-    player_input_valid = False
-    while(player_input_valid != False):
-        player_input_str = get_player_input()
-        player_input_valid = validate_player_input(player_input_str)
+        stats["player"] += ["lost"]
+        stats["computer"] += ["won"]
 
 # update rounds
-def round_counter():
-    rounds_played = 0
-    rounds_played += 1
-    return rounds_played
+def round_counter(stats):
+    stats["rounds"] += 1
 
 # print final stats
-def print_stats():
+def print_stats(stats, scores, rounds_played):
     print("GAME OVER!\n")
-    print("This game lasted " + str(rounds_played) + " rounds.\n")
-    print("You scored " + str(player_score) + " and the computer scored " + str(computer_score) + ".\n")
-    print("Your stats were:")
-    print(player_stats)
-    print("The computer's stats were:")
-    print(computer_stats)
+    print("This game lasted " + str(stats["rounds"]) + " rounds.\n")
+    print("You scored " + str(scores["player"]) + " and the computer scored " + str(scores["computer"]) + ".\n")
+    print("Your stats were: " + str(stats["player"]))
+    print("The computer's stats were: " + str(stats["computer"]))
+    print(stats["computer"])
+
+# run game loop
+def run_game(winning_total):
+    scores = {"player": 0, "computer": 0}
+    stats = {"player": [], "computer": [], "rounds": 0}
+
+    while is_game_complete(scores, winning_total):
+        player_input_str = get_player_input()
+        if not validate_player_input(player_input_str):
+            print("invalid response")
+            continue
+        update_scores(scores, 2)
+        print("running")
+
+run_game(3)
+print("Game over")
