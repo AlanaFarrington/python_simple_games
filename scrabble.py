28ae56e3e1d0b27f09c7e70f9c60465582251c3c
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-10-21 13:52:04 +0100

    extending scrabble game function

--- scrabble.py
+++ scrabble.py
@@ -2,7 +2,12 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 letter_to_points = {key:value for key, value in zip(letters, points)}
 letter_to_points[" "] = 0
-print(letter_to_points)
+#print(letter_to_points)
+
+player_1 = "player_1"
+player_2 = "player_2"
+player_1_words = []
+player_2_words = []
 
 def score_word(word):
   word_characters = [*word]
@@ -11,22 +16,30 @@ def score_word(word):
     point_total += letter_to_points.get(letter, 0)
   return point_total
 
-brownie_points = score_word("BROWNIE")
-print(brownie_points)
-story_points = score_word("STORY")
-print(story_points)
+# play_word() — a function that would take in a player and a word, and add that word to the list of words they’ve played
+def play_word(player, word):
+  if player == "player_1":
+    player_1_words.append(word)
+  if player == "player_2":
+    player_2_words.append(word)
 
-player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+#brownie_points = score_word("BROWNIE")
+#print(brownie_points)
+#story_points = score_word("STORY")
+#print(story_points)
+
+player_words = {"player_1": player_1_words, "player_2": player_2_words}
 player_to_points = {}
-for player, words in player_to_words.items():
+for player, words in player_words.items():
   player_points = 0
   for word in words:
     player_points += score_word(word)
   player_to_points[player] = player_points
 print(player_to_points)
 
-# If you want extended practice, try to implement some of these ideas with the Python you’ve learned:
-# play_word() — a function that would take in a player and a word, and add that word to the list of words they’ve played
+# If you want extended practice, try to implement some of these ideas with the Python you’ve learned
 # update_point_totals() — turn your nested loops into a function that you can call any time a word is played
 # make your letter_to_points dictionary able to handle lowercase inputs as well
+# check if inputted words are real using a dictionary scraper
+# plan out a board as a simple UI
 
--- test.py
+++ test.py
@@ -1,48 +1,11 @@
-import random
-rounds_played = 0
-game_choices = {1: "rock", 2: "paper", 3: "scissors"}
-player_stats = []
-computer_stats = []
-computer_score = 0
-player_score = 0
 
-def compare_choices():
-    global computer_score
-    global player_score
-    player_choice = int(input("Make your choice. rock = 1, paper = 2 or scissors = 3? "))
-    print("You chose " + game_choices[player_choice])
-    computer_choice = random.randint(1,3)
-    print("The computer chose " + game_choices[computer_choice])
-    if player_choice == computer_choice:
-            print("You drew this round.")
-            player_score += 1
-            computer_score += 1
-            player_stats.append("drew")
-            computer_stats.append("drew")
-    elif (player_choice == 1 and computer_choice == 3) or (player_choice == 2 and computer_choice == 1) or (player_choice == 3 and computer_choice == 2):
-        print("You won!")
-        player_score += 1
-        player_stats.append("won")
-        computer_stats.append("lost")
-    else:
-        print("You lost!")
-        computer_score += 1
-        player_stats.append("lost")
-        computer_stats.append("won")
+word_underscore_list = ["c", "a", "t"]
+word_list = ["c", "a", "t"]
 
-# Make game loop run 3 times
-def game_loop():
-    while (player_score < 3) or (computer_score < 3):
-        rounds_played += 1
-        print("Round " + str(rounds_played))
-        compare_choices()
+def is_game_complete(word_list, word_underscore_list):
+    if word_list == word_underscore_list:
+        return False
+    return True
 
-#print final score list
-print("Game over!")
-print("This game lasted " + str(rounds_played) + " rounds.")
-print("You scored " + str(player_score) + " and the computer scored " + str(computer_score))
-print("Your stats were:")
-print(player_stats)
-print("The computer's stats were:")
-print(computer_stats)
+print(is_game_complete(word_list, word_underscore_list))
 
