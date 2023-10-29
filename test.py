d337c64b13b863cf25ac843b379fc1aff9328a7f
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-10-29 23:41:28 +0000

    testing scrabble game functions

--- test.py
+++ test.py
@@ -1,11 +1,29 @@
+import random
 
-word_underscore_list = ["c", "a", "t"]
-word_list = ["c", "a", "t"]
+# make dictionary of letter points
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10, 0]
+letter_points = {key:value for key, value in zip(letters, points)}
 
-def is_game_complete(word_list, word_underscore_list):
-    if word_list == word_underscore_list:
-        return False
-    return True
+# make list of remaining letter tiles
+remaining_letter_tiles = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z", " ", " "]
+# print(letter_points)
 
-print(is_game_complete(word_list, word_underscore_list))
+#set players letter tiles and played words as empty lists
+player_1_letters = []
+player_2_letters = []
 
+# fill player tiles rack
+def fill_player_tiles(player_letters, remaining_letter_tiles):
+  while len(player_letters) < 7:
+    select_random_tile = random.randint(0,99)
+    print(select_random_tile)
+    player_letters.append(remaining_letter_tiles[select_random_tile])
+    # remove used letter from remaining_letter_tiles list
+    #remaining_letter_tiles.pop[select_random_tile]
+    continue
+  return player_letters
+
+player_1_tiles = fill_player_tiles(player_1_letters, remaining_letter_tiles)
+print(player_1_tiles)
+print(remaining_letter_tiles)
