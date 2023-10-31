85bccba124947f56841e9841a14d5ff15e0ab1f1
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-10-31 18:14:55 +0000

    further testing on updating letter list function

--- test.py
+++ test.py
@@ -7,55 +7,44 @@ letter_points = {key:value for key, value in zip(letters, points)}
 
 # make list of remaining letter tiles
 remaining_letter_tiles = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z", " ", " "]
-# print(letter_points)
 
-#set players letter tiles and played words as empty lists
-player_1_letters = ["S", "A"]
-player_2_letters = ["B", "N", "P"]
+player_1 = "Alana"
+player_2 = "Will"
+player_letters = {player_1: ["S", "A", "E"], player_2: ["B", "N", "P", "E", "R", "S", "E"]}
+player_words = {player_1: ["SEA"], player_2: ["BROWNIE"]}
 
-# fill player tiles rack
-def fill_player_tiles(player_letters, remaining_letter_tiles):
-  while len(player_letters) < 7:
-    select_random_tile = random.randint(0,len(remaining_letter_tiles))
-    print(select_random_tile)
-    player_letters.append(remaining_letter_tiles[select_random_tile])
+# fill player tiles rack with 7 random letter tiles from the 100 tiles which are then removed from the remaining tiles list - TESTED
+def fill_player_tiles(player):
+  while len(player_letters[player]) < 7:
+    select_random_tile = random.randint(0,(len(remaining_letter_tiles) - 1))
+    player_letters[player].append(remaining_letter_tiles[select_random_tile])
     # remove used letter from remaining_letter_tiles list
     del remaining_letter_tiles[select_random_tile]
     continue
-  return player_letters
 
-# print player letter list
-def print_letter_list(player, player_letters):
+# print player letter list - TESTED
+def print_letter_list(player):
    print(player + ", your letters are:")
-   print(player_letters)
+   print(player_letters[player])
 
-# get player input
+# get player input - TESTED
 def get_player_input():
     player_input = input("What is your word to play? ")
+    print("Your word was " + (player_input.upper()) + ".")
     return player_input.upper()
 
-# validate player input - checked against their available letter tiles
-def is_valid_word(player_letters, word):
-   word_characters = [*word]
-   print(len(word_characters))
-   # check if word is correct length
-   if len(word_characters) < 2 or len(word_characters) > 7:
-      print("invalid word length. Word must be between 2 and 7 letters long.")
-      return False
-   #compare word characters with player letters
-   check_letters = all(item in player_letters for item in word_characters)
-   if check_letters == False:
-    print("Invalid letter selection. Your played word contains one or more letters that you do not have.")
-    return False
-   return True
-
+# update players letter tile list after word has been played - TESTED
+def update_player_letter_list(valid_word, player, word):
+   if valid_word == True:
+      for letter in [*word]:
+         player_letters[player].remove(letter)
+   # replace used letter with new letters
+   fill_player_tiles(player)
 
 # test calls of functions
-fill_player_tiles(player_1_letters, remaining_letter_tiles)
-print_letter_list("Player 1", player_1_letters)
-print(len(remaining_letter_tiles))
-
+fill_player_tiles(player_1)
+print_letter_list(player_1)
 player_1_word = get_player_input()
-print(player_1_word)
-is_valid = is_valid_word(player_1_letters, player_1_word)
-print(is_valid)
+#print(player_1_word)
+is_valid = True
+update_player_letter_list(is_valid, player_1, player_1_word)
